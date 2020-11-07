c7d30212f79a7819cf8bc60c66a7bccae9821cbf
From Server <fromserver@test.test>
2020-11-07 23:03:21 +0000

    add the rest

---
+++ default.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="uk">
+
+<head>
+    <title>Default</title>
+    <meta charset="utf-8">
+    <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">
+    <meta content="Default" name="description">
+    <meta content="width=device-width, initial-scale=1" name="viewport">
+</head>
+
+<body>
+We will be available in a few days!
+</body>
+
+</html>
